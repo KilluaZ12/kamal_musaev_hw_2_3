378b9391b8268706bbbac3452aaf7576327457d3
KilluaZ12 <kamalmusaev.12.92@gmail.com>
2023-04-12 15:28:36 +0600

    initial commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public BankAccount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+    }
+
+    public void withdraw(double sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Withdrawal amount exceeds the account balance", amount);
+        }
+        amount -= sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount(20000.0);
+
+        try {
+            while (true) {
+                account.withdraw(6000.0);
+            }
+        } catch (LimitException e) {
+            double remainingAmount = e.getRemainingAmount();
+            System.out.println("Remaining amount: " + remainingAmount);
+            try {
+                account.withdraw(remainingAmount);
+            } catch (LimitException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        System.out.println("Final balance: " + account.getAmount());
     }
 }
